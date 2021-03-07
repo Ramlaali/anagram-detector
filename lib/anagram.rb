09374dd65a1c86a229a5132957d43c8a7806f0c2
Ramla <ramlaali.abd@gmail.com>
2021-03-07 05:31:03 -0500

    commit tree 36a244c0cc4b74a087677fa514dcbb2369960f45

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :words
+    
+    def initialize(words)
+        @words = words
+        @word = @words.split("").sort
+    end
+
+    def match(anagram_output)
+        anagram_output.select do |anagram_output|
+            @word == anagram_output.split("").sort
+        end
+    end
+    
+end
